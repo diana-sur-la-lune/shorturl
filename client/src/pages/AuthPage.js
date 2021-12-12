dd6fcf7b5dd1d2f893394fe5180e11332492c45a
Diana <dia.voinova@gmail.com>
2021-12-12 18:26:50 +0200

    authorisation

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useHttp } from '../hooks/http.hook'
 import {useMessage} from '../hooks/message.hook'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 // import { Navigate, Outlet } from 'react-router-dom'
 
 export const AuthPage = () => {
+    const auth = useContext(AuthContext)
     const message = useMessage()
     const {loading, request, error, clearError} = useHttp()
     const [form, setForm] = useState({
@@ -26,6 +29,13 @@ message(data.message)
     } catch(e) {}
 }
 
+const loginHandler = async () => {
+    try {
+const data = await request('/api/auth/login', 'POST', {...form})
+auth.login(data.token, data.userId)
+    } catch(e) {}
+}
+
     return (
         <div className="row">
           <div className="col s6 offset-s3">
@@ -35,7 +45,7 @@ message(data.message)
           <span className="card-title">Transform your long URL into a shorter, more readable link</span>
         <div>
 
-        <div class="input-field" style={{marginTop: 40}}>
+        <div className="input-field" style={{marginTop: 40}}>
           <input
           placeholder="Enter your email"
           id="email"
@@ -44,10 +54,10 @@ message(data.message)
           className="lime-input"
           onChange={changeHandler}
            />
-          <label for="email">Email</label>
+          <label htmlFor="email">Email</label>
         </div>
 
-        <div class="input-field">
+        <div className="input-field">
           <input
           placeholder="Enter your password"
           id="password"
@@ -56,7 +66,7 @@ message(data.message)
           className="lime-input"
           onChange={changeHandler}
            />
-          <label for="password" >Password</label>
+          <label htmlFor="password" >Password</label>
         </div>
 
         </div>
@@ -66,6 +76,7 @@ message(data.message)
           className="btn lime"
           style={{marginRight: 10}}
           disabled={loading}
+          onClick={loginHandler}
           >Log In</button>
           <button
           className="btn grey lighten-1 black-text"
