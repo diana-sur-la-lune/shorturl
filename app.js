14b88b670cba2be391f13efc3974d9c046c6d32b
Diana <dia.voinova@gmail.com>
2021-12-12 22:24:18 +0200

    links

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./routes/auth.routes'))
+app.use('api/link', require('./routes/link.routes'))
 
 const PORT = config.get('port') || 5000
 
@@ -14,16 +15,16 @@ const PORT = config.get('port') || 5000
 async function start () {
 try {
  await mongoose.connect(config.get('mongoUri'), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
  })
+ app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
  } catch (e) {
     console.log('Server Error', e.message)
     process.exit(1) 
  }
 }
 
-
 start()
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+
+
